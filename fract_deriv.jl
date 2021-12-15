44f45516dba3d1c5db0d5c7e742b89ca2044cfab
17thSaint <pgeraghty3@outlook.com>
2021-12-15 15:14:57 +0100

    working now

--- fract_deriv.jl
+++ fract_deriv.jl
@@ -1,37 +1,35 @@
-using SpecialFunctions,PyPlot,CurveFit
+#using SpecialFunctions,PyPlot,CurveFit
 
 function get_fractderiv(h,delta_t,steps,og_func,noise_steps,lim_val=0.1)
 	fract_deriv = [0.0 for i in 1:steps-1]
-	#times = [0.0 for i in 1:steps-1]
 	for i in 1:steps-1
-		#times[i] = i*delta_t
-		noise_times = append!([0.0],[ l*delta_t + j*delta_t/noise_steps for l in 0:i-1 for j in 1:noise_steps])
-		for j in 1:length(noise_times)-1
-			func_here = og_func[j]
-			binom_part = gamma(3-2*h)/(factorial(j)*gamma(3-2*h-j))
-			fract_deriv[i] += ((-1)^(j))*binom_part*func_here
+		for j in 0:noise_steps*i
+			func_here = og_func[noise_steps*i-j+1]
+			fact_j = exp(sum([log(i) for i in 1:j]))
+			binom_part = gamma(3-2*h)/(fact_j*gamma(3-2*h-j))
+			fract_deriv[i] += ((-1)^(j))*binom_part*func_here/(delta_t/noise_steps)^(2-2*h)
 		end
 	end
-	return fract_deriv#,times
+	return fract_deriv
 end
 
 
-dt = 1.0
-time_steps = 5
+time_steps = 10
+dt = 1/time_steps
 h = 0.3
 alpha = 2-2*h
 times = [i*dt for i in 1:time_steps-1]
 
-noises = [1,2,3]
+noises = [10]
 #power_vals = [0.0 for j in 1:length(hs)]
-pow = 3
+pow = 2
 for j in 1:length(noises)
 	noise_steps = noises[j]
-	og_func = [(t*dt)^pow for t in 1:time_steps*noise_steps]
+	#noise_times = [i*dt/noise_steps for i in 1:time_steps*noise_steps]
+	og_func = [(t*dt/noise_steps)^pow for t in 1:time_steps*noise_steps]
 	rezz = get_fractderiv(h,dt,time_steps,og_func,noise_steps)
 	plot(times,rezz,label="Steps=$noise_steps")
-	#fit_vals = power_fit(rezz[2],rezz[1])
-	#power_vals[j] = 0.5*(fit_vals[2]+2-pow)
+	#plot(noise_times,og_func,label="Steps=$noise_steps")
 end
 theory = [(factorial(pow)*times[i]^(pow-alpha))/gamma(pow+1-alpha) for i in 1:time_steps-1]
 plot(times,theory,label="Theory")
